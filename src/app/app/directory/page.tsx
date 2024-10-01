b258d863fd1799daebffc49e7a7fe71dd4c6a6ed
joshuasilva414 <joshuasilva414@gmail.com>
2024-10-01 11:00:20 -0500

    add directory search input

--- src/app/app/directory/page.tsx
+++ src/app/app/directory/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { getAllOfficers } from "@/db/functions/officers";
 import OfficerCard from "@/components/directory/officer-card";
+import { Input } from "@/components/ui/input";
 
 export default async function Page() {
   const { user } = await validateRequest();
@@ -14,7 +15,7 @@ export default async function Page() {
   return (
     <main className="w-full mx-auto page-wrap max-w-5xl flex flex-col gap-y-5">
       <h1 className="font-black text-5xl pb-10">Directory</h1>
-      <Bubble title="Quick Links" className="flex gap-x-2">
+      {/* <Bubble title="Quick Links" className="flex gap-x-2">
         <Link href={"https://notion.so/acmutsa"}>
           <Button>Notion</Button>
         </Link>
@@ -27,6 +28,13 @@ export default async function Page() {
         <Link href={"https://wiki.acmutsa.org/"}>
           <Button>ACM Wiki</Button>
         </Link>
+      </Bubble> */}
+      <Bubble title="Search" className="flex">
+        <Input
+          type="search"
+          className="max-w-sm"
+          placeholder="John Doe"
+        ></Input>
       </Bubble>
       <div className="grid grid-cols-3 grid-flow-col">
         {allOfficers.map((o) => (
